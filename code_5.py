097ded8711476f70258e2e196202ce6697151eea
Eduardo (David) Villacis Calderon <villacis@vt.edu>
2023-02-04 19:03:22 -0500

    Updating testing and code template

--- code_5.py
+++ code_5.py
@@ -1,7 +1,13 @@
 
-def main():
-   pass
+import random
+def main(computer_option = None):
+   options_dictionary = {1:"Rock",2:"Paper",3:"Scissors"}
+   if computer_option is None:
+      computer_option = random.randint(1,3)
+   computer_choice = options_dictionary[computer_option]
+   # Implement your code HERE. Usen computer_choice to obtain computer play
+   
     
-
+# Do not remove the lines below
 if __name__ == "__main__":
     main()
